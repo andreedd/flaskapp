f1d861b6e4016e2582d7ec8085c9ccfb8b042bf3
andreedd <ddeeh2@gmail.com>
2020-07-03 14:36:13 +0300

    rm

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
 @app.route('/')
 def hello_world():
     return 'Flaskapp'
